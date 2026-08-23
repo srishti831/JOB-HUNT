39d9f4f3074331ae43aa545a0b367eccf33ba2ab
Srishti Singh <srishti.20233278@mnnit.ac.in>
2026-08-24 00:42:36 +0530

    main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -7,12 +9,12 @@ import {Provider} from "react-redux";
 import store from "./redux/store.js";
 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Provider store={store}>
        <App />
     </Provider>
    
     <Toaster/>
-  </StrictMode>
+  </React.StrictMode>
 );
