c17931bad4476f7bf086366310c9df23bd5af80c
Srishti Singh <srishti.20233278@mnnit.ac.in>
2026-08-23 22:03:08 +0530

    login and singup pages

---
+++ frontend/src/components/ui/shared/navbar.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { User2, LogOut } from "lucide-react";
+
+export const Navbar = () => {
+  const user = false;
+  return (
+    <div classname="bg-white">
+      <div className="flex item-center justify-between mx-auto max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Job<span className="text-[#F83002]">Portal</span>
+          </h1>
+        </div>
+        <div className="flex items-center gap-12 ">
+          <ul className="flex font-medium items-center gap-5">
+            <li>Home</li>
+            <li>Jobs</li>
+            <li>Browse</li>
+          </ul>
+          {!user ? (
+            <div className = 'flex items-center gap-2'>
+              <Button variant="outline">Login</Button>
+              <Button className = "bg-[#6a4aa6] hover:bg-[#39176f]">SignUp</Button>
+            </div>
+          ) : (
+            <Popover>
+              <PopoverTrigger asChild>
+                <Avatar className="curson-pointer">
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="@shadcn"
+                    className="grayscale"
+                  />
+                </Avatar>
+              </PopoverTrigger>
+              <PopoverContent className="w-80">
+                <div className="flex gap-4 space-y-2">
+                  <Avatar className="curson-pointer">
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                      className="grayscale"
+                    />
+                  </Avatar>
+                  <div>
+                    <h4 className="font-medium">Srishti Singh</h4>
+                    <p className="text - sm text-muted-foreground">
+                      This is my profile
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col my-2 text-gray-600">
+                  <div classNawe="flex w-fit items-center gap-2 cursor-pointer">
+                    <User2 />
+                    <Button variant="link">View Profile</Button>
+                  </div>
+
+                  <div classNawe="flex w-fit items-center gap-2 cursor-pointer">
+                    <LogOut />
+                    <Button variant="link">Logout</Button>
+                  </div>
+                </div>
+              </PopoverContent>
+            </Popover>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
